14a7b28e99ab149b2fb55c7b4c556d465f7e2099
Live61c5 <stephane.nomine@gmail.com>
2023-03-08 14:21:18 +0100

    some changes

--- private/models/productModel.php
+++ private/models/productModel.php
@@ -95,3 +95,93 @@ function getProductsByPriceAndSearchAndOrder($min, $max, $search, $order)
     $req->execute(array($min, $max, '%' . $search . '%'));
     return $req;
 }
+
+
+// Action pour afficher tous les produits
+function showProducts()
+{
+    $products = getProducts();
+    include 'readProducts.php';
+}
+
+// Action pour afficher un produit spécifique en fonction de son ID
+function showProduct($id)
+{
+    $product = getProduct($id);
+    include 'readProduct.php';
+}
+// Action pour afficher les produits par catégorie
+function showProductsByCategory($id)
+{
+    $products = getProductsByCategory($id);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par recherche
+function showProductsBySearch($search)
+{
+    $products = getProductsBySearch($search);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par prix
+function showProductsByPrice($min, $max)
+{
+    $products = getProductsByPrice($min, $max);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par prix et catégorie
+function showProductsByPriceAndCategory($min, $max, $id)
+{
+    $products = getProductsByPriceAndCategory($min, $max, $id);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par prix et recherche
+function showProductsByPriceAndSearch($min, $max, $search)
+{
+    $products = getProductsByPriceAndSearch($min, $max, $search);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par catégorie et recherche
+function showProductsByCategoryAndSearch($id, $search)
+{
+    $products = getProductsByCategoryAndSearch($id, $search);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par prix, catégorie et recherche
+function showProductsByPriceAndCategoryAndSearch($min, $max, $id, $search)
+{
+    $products = getProductsByPriceAndCategoryAndSearch($min, $max, $id, $search);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par prix, catégorie, recherche et ordre
+function showProductsByPriceAndCategoryAndSearchAndOrder($min, $max, $id, $search, $order)
+{
+    $products = getProductsByPriceAndCategoryAndSearchAndOrder($min, $max, $id, $search, $order);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par prix, catégorie et ordre
+function showProductsByPriceAndCategoryAndOrder($min, $max, $id, $order)
+{
+    $products = getProductsByPriceAndCategoryAndOrder($min, $max, $id, $order);
+    include 'readProducts.php';
+}
+// Action pour afficher les produits par prix, recherche et ordre
+function showProductsByPriceAndSearchAndOrder($min, $max, $search, $order)
+{
+    $products = getProductsByPriceAndSearchAndOrder($min, $max, $search, $order);
+    include 'readProducts.php';
+}
+// Action pour supprimer un produit en fonction de son ID
+function deleteProduct($id)
+{
+    // Vérifiez si le formulaire de suppression a été soumis
+    if (isset($_POST['delete'])) {
+        // Supprimez le produit du modèle
+        deleteProductById($id);
+
+        // Redirigez vers l'action d'affichage des produits après la suppression
+        header("Location: index.php?action=showProducts");
+    } else {
+        // Affichez la vue de confirmation de suppression
+        include 'deleteProduct.php';
+    }
+}
